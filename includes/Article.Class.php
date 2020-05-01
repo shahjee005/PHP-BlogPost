14cf9eb83b15cf96c34fb8adb613468a8a1cfa89
Syed-Gardezi <61136718+shahjee005@users.noreply.github.com>
2020-05-01 09:46:01 -0600

    fixed the errors

--- includes/Article.Class.php
+++ includes/Article.Class.php
@@ -4,14 +4,14 @@ class Article {
     public $title = '';
     public $content  = '';
   
-  function __construct ( $id = 0, $title = 'No title', $content = 'Nothing' )
+  function __construct ( $id = 00, $title = 'No title', $content = 'Nothing' )
   {
     if ( is_integer( $id ) && !empty( $id ) )
       $this->id = $id;
     if ( is_string( $title ) && !empty( $title ) )
-      $this->price = $title;
+      $this->title = $title;
     if ( is_string( $content ) && !empty( $content ) )
-      $this->type = $content;
+      $this->content = $content;
   }
   // Outputs a Article.
   public function output ( $echo = TRUE )
